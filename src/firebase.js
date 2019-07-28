75537a8b5a697d29f2080a051bae327f9746cacf
hsouiri97 <53407457+hsouiri97@users.noreply.github.com>
2019-07-29 00:01:55 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'; 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCjHNAcbmKoByfz_b7k9__o9uS-06ixn0A",
-    authDomain: "vue-firebase-ecommerce.firebaseapp.com",
-    databaseURL: "https://vue-firebase-ecommerce.firebaseio.com",
-    projectId: "vue-firebase-ecommerce",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "1019118550352",
-    appId: "1:1019118550352:web:e0531361f723354f"
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   export const fb = firebase.initializeApp(firebaseConfig);
